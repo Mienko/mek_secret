4959076480bebfc366c190b684fde066cd649e44
misund <thomas@hemmeligadresse.com>
2014-08-21 20:57:40 +0000

    Flexboxing the entry-meta.

--- content-footer.php
+++ content-footer.php
@@ -6,7 +6,7 @@
 				if ( $categories_list && mek_secret_categorized_blog() ) :
 			?>
 			<div class="cat-links">
-				<?php printf( __( 'Posted in %1$s', 'mek_secret' ), $categories_list ); ?>
+				<?php printf( __( 'Posted in<br>%1$s', 'mek_secret' ), $categories_list ); ?>
 			</div>
 			<?php endif; // End if categories ?>
 
@@ -17,7 +17,7 @@
 				if ( $tags_list ) :
 			?>
 			<div class="tags-links <?php echo 1 < count( $tags ) ? 'plural-tags' : 'single-tag'; ?>">
-				<?php printf( __( 'Tagged %1$s', 'mek_secret' ), $tags_list ); ?>
+				<?php printf( __( 'Tagged<br>%1$s', 'mek_secret' ), $tags_list ); ?>
 			</div>
 			<?php endif; // End if $tags_list ?>
 
@@ -27,7 +27,7 @@
 
 			<div class="permalink">
 				<?php printf( 
-					__( 'Bookmark the <a href="%1$s" rel="bookmark">permalink</a>.', 'mek_secret' ),
+					__( 'Bookmark<br>the <a href="%1$s" rel="bookmark">permalink</a>.', 'mek_secret' ),
 					get_permalink()
 				); ?>
 			</div>
@@ -35,8 +35,8 @@
 		<?php endif; // End if is_single() ?>
 
 		<?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
-		<div class="comments-link <?php echo 1 < get_comments_number() ? 'plural-comments' : ''; ?>"> <?php comments_popup_link( __( 'Leave a comment', 'mek_secret' ), __( '1 Comment', 'mek_secret' ), __( '% Comments', 'mek_secret' ) ); ?></div>
+		<div class="comments-link <?php echo 1 < get_comments_number() ? 'plural-comments' : ''; ?>"><br><?php comments_popup_link( __( 'Leave a comment', 'mek_secret' ), __( '1 Comment', 'mek_secret' ), __( '% Comments', 'mek_secret' ) ); ?></div>
 		<?php endif; ?>
 
-		<?php edit_post_link( __( 'Edit', 'mek_secret' ), '<span class="edit-link"> ', '</span>' ); ?>
+		<?php edit_post_link( __( 'Edit', 'mek_secret' ), '<span class="edit-link"><br>', '</span>' ); ?>
 	</footer><!-- .entry-footer -->
