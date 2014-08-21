c1fdddf4fd44c938b961129697362e349741f86c
misund <thomas@hemmeligadresse.com>
2014-08-21 16:17:48 +0000

    Adding icons here and there.

--- content.php
+++ content.php
@@ -25,33 +25,6 @@
 		?>
 	</div><!-- .entry-content -->
 
-	<footer class="entry-footer">
-		<?php if ( 'post' == get_post_type() ) : // Hide category and tag text for pages on Search ?>
-			<?php
-				/* translators: used between list items, there is a space after the comma */
-				$categories_list = get_the_category_list( __( ', ', 'mek_secret' ) );
-				if ( $categories_list && mek_secret_categorized_blog() ) :
-			?>
-			<span class="cat-links">
-				<?php printf( __( 'Posted in %1$s', 'mek_secret' ), $categories_list ); ?>
-			</span>
-			<?php endif; // End if categories ?>
+<?php get_template_part( 'content-footer' ); ?>
 
-			<?php
-				/* translators: used between list items, there is a space after the comma */
-				$tags_list = get_the_tag_list( '', __( ', ', 'mek_secret' ) );
-				if ( $tags_list ) :
-			?>
-			<span class="tags-links">
-				<?php printf( __( 'Tagged %1$s', 'mek_secret' ), $tags_list ); ?>
-			</span>
-			<?php endif; // End if $tags_list ?>
-		<?php endif; // End if 'post' == get_post_type() ?>
-
-		<?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
-		<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'mek_secret' ), __( '1 Comment', 'mek_secret' ), __( '% Comments', 'mek_secret' ) ); ?></span>
-		<?php endif; ?>
-
-		<?php edit_post_link( __( 'Edit', 'mek_secret' ), '<span class="edit-link">', '</span>' ); ?>
-	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
--- footer.php
+++ footer.php
@@ -12,7 +12,7 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'mek_secret' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'mek_secret' ), 'WordPress' ); ?></a>
+			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'mek_secret' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'mek_secret' ), '<span class="wp">WordPress</span>' ); ?></a>
 			<span class="sep"> | </span>
 			<?php printf( __( 'Theme: %1$s by %2$s.', 'mek_secret' ), 'Secret', '<a href="http://mienko.no/" rel="designer">Mienko</a>' ); ?>
 		</div><!-- .site-info -->
