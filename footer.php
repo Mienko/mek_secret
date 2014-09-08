5961ed86237f8534ffa350217179baa399327669
misund <thomas@hemmeligadresse.com>
2014-09-08 18:12:15 +0200

    In footer, adding Github link and Mienko colors.

--- footer.php
+++ footer.php
@@ -12,9 +12,9 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'mek_secret' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'mek_secret' ), '<span class="wp">WordPress</span>' ); ?></a>
+			<?php printf( __( 'Proudly powered by %s', 'mek_secret' ), '<a href="' . esc_url( 'http://wordpress.org/' ) . '"><span class="wp">WordPress</span></a>' ); ?>
 			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'mek_secret' ), 'Secret', '<a href="' . esc_url( 'http://mienko.no/' ) . '" rel="designer">Mienko</a>' ); ?>
+			<?php printf( __( 'Theme: %1$s by %2$s.', 'mek_secret' ), '<a href="' . esc_url( 'https://github.com/Mienko/mek_secret/' ) . '">' . __( 'Secret' , 'mek_secret' ) . '</a>' , '<a href="' . esc_url( 'http://mienko.no/' ) . '" rel="designer">Mienko</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
