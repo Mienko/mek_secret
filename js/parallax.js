69c9d836332d4097e27f3d23a72e0665fc5501f6
misund <thomas@hemmeligadresse.com>
2014-09-25 21:04:11 +0200

    Don't break the parallax function if the image is shorter than the header.

--- js/parallax.js
+++ js/parallax.js
@@ -9,7 +9,12 @@ jQuery( document ).ready( function( $ ){
 			// Calculate the offset in pixels
 			parallaxPx = ( scroll / 2 ) + ( bgHeight - headerHeight ) / 2;
 			// Because of background-size:cover, the offset should be given in percentages instead of pixels
-			parallaxPct = 100-( 100*parallaxPx / (bgHeight-headerHeight) );
+			if ( bgHeight > headerHeight )
+				parallaxPct = 100-( 100*parallaxPx / (bgHeight-headerHeight) );
+			else if ( bgHeight < headerHeight )
+				parallaxPct = ( 100*parallaxPx / (bgHeight-headerHeight) );
+			else
+				parallaxPct = 50-( 100*parallaxPx / (headerHeight) );
 
 			img.css( 'background-position' , '50% ' + parallaxPct + '%' );
 		}
